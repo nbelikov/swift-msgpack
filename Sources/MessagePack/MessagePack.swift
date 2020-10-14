9e842ed98862091d5f66c5446f723ad254e8d192
Nikolay Belikov <nb@nbelikov.com>
2020-10-14 10:56:29 +0300

    Implement FormatByte
    FormatByte is an abstraction over one byte long prefix that specifies
    the type of the following value

--- Sources/MessagePack/MessagePack.swift
+++ Sources/MessagePack/MessagePack.swift
@@ -1,3 +1,72 @@
-struct MessagePack {
-    var text = "Hello, World!"
+import Foundation
+
+struct FormatByte: RawRepresentable {
+    let format: FormatSpecifier
+    let value:  UInt8
+
+    enum FormatSpecifier: UInt8 {
+        case positiveFixint = 0x00 // 0xxxxxxx  0x00 - 0x7f
+        case fixmap         = 0x80 // 1000xxxx  0x80 - 0x8f
+        case fixarray       = 0x90 // 1001xxxx  0x90 - 0x9f
+        case fixstr         = 0xa0 // 101xxxxx  0xa0 - 0xbf
+        case `nil`          = 0xc0
+        // 0xc1 is an invalid value
+        case bool           = 0xc2 // 1100001x  0xc2 - 0xc3
+        case bin8     = 0xc4, bin16   = 0xc5, bin32   = 0xc6
+        case ext8     = 0xc7, ext16   = 0xc8, ext32   = 0xc9
+        case float32  = 0xca, float64 = 0xcb
+        case uint8    = 0xcc, uint16  = 0xcd, uint32  = 0xce, uint64  = 0xcf
+        case int8     = 0xd0, int16   = 0xd1, int32   = 0xd2, int64   = 0xd3
+        case fixext1  = 0xd4, fixext2 = 0xd5, fixext4 = 0xd6, fixext8 = 0xd7
+        case fixext16 = 0xd8
+        case str8     = 0xd9, str16   = 0xda, str32   = 0xdb
+        case array16  = 0xdc, array32 = 0xdd
+        case map16    = 0xde, map32   = 0xdf
+        case negativeFixint = 0xe0 // 111xxxxx  0xe0 - 0xff
+
+        var maximalValue: UInt8 {
+            switch self {
+            case .positiveFixint: return 0x7f // See bit patterns above
+            case .fixmap:         return 0x0f
+            case .fixarray:       return 0x0f
+            case .fixstr:         return 0x1f
+            case .bool:           return 0x01
+            case .negativeFixint: return 0x1f
+            default:              return 0x00
+            }
+        }
+    }
+
+    init(format: FormatSpecifier) {
+        precondition(format.maximalValue == 0)
+        self.format = format
+        self.value  = 0
+    }
+
+    init(format: FormatSpecifier, withValue value: UInt8) {
+        precondition(format.maximalValue > 0)
+        precondition(value <= format.maximalValue)
+        self.format = format
+        self.value  = value
+    }
+
+    init?(rawValue: UInt8) {
+        let format: FormatSpecifier?
+        switch rawValue {
+        case 0x00...0x7f: format = .positiveFixint
+        case 0x80...0x8f: format = .fixmap
+        case 0x90...0x9f: format = .fixarray
+        case 0xa0...0xbf: format = .fixstr
+        case 0xc2...0xc3: format = .bool
+        case 0xe0...0xff: format = .negativeFixint
+        default:          format = FormatSpecifier(rawValue: rawValue)
+        }
+        if format == nil { return nil }
+        self.format = format!
+        self.value  = rawValue ^ format!.rawValue
+    }
+
+    var rawValue: UInt8 {
+        return self.format.rawValue ^ self.value
+    }
 }
